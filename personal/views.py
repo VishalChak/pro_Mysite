39698930ab9b5078a2e5a23de1b6492ded5f0b55
VishalChak <vishalbabu.in@gmail.com>
2017-05-14 23:31:09 +0530

    Learn template

--- personal/views.py
+++ personal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request,'personal/home.html')
+	return render(request,'personal/index.html')
