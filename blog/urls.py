d887a944281e8a61b24697912dc91309cef5803a
VishalChak <vishalbabu.in@gmail.com>
2017-05-15 12:40:28 +0530

    Current

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url,include
-from django.views.generic import ListView,DetailViews
-from blog.models import Post
-
-urlpatterns = [
-	url(r'^$',ListView.asview(queryset=Post.objects.all().order_by("-date")[:25], tamplate_name = 'blog/blog.html'))]
-
-
--- personal/views.py
+++ personal/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request,'personal/home.html')
+	return render(request,'personal/index.html')
 def contact(request):
 	return render(request,'personal/basic.html',{'content':['If you would like to contact me, please email me','vishalbabu.in@gmail.com']})
